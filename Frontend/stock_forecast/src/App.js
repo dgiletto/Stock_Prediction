0a9f97d0a0b55dc44029d7979fe171147d141629
dgiletto <dgiletto@udel.edu>
2025-06-19 18:15:35 -0400

    API Added

--- Frontend/stock_forecast/src/App.js
+++ Frontend/stock_forecast/src/App.js
@@ -9,7 +9,7 @@ function App() {
 
   const getForecast = async () => {
     try {
-      const res = await axios.get(`http://localhost:8000/forecast/${ticker}`);
+      const res = await axios.get(`https://stock-prediction-dluq.onrender.com/forecast/${ticker}`);
       if (res.data.forecast) {
         setForecast(res.data.forecast);
         setError('');
