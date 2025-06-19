c25e589ed189f8459924cb9b1c8e88efd669fc27
dgiletto <dgiletto@udel.edu>
2025-06-19 16:45:18 -0400

    Orgaization + Create React App

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from forecast import forecast_7_day
+from Backend.forecast import forecast_7_day
 
 app = FastAPI()
 
